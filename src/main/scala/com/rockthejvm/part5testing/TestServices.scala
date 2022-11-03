2eb93847b80aeeb3bd21eab29aa2532d416b5ad9
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-11-03 10:44:17 +0100

    Testing Services and Dependencies

---
+++ src/main/scala/com/rockthejvm/part5testing/TestServices.scala
@@ -0,0 +1,133 @@
+package com.rockthejvm.part5testing
+
+import zio.*
+import zio.test.*
+import zio.test.Assertion.*
+import zio.test.TestAspect.*
+
+object SimpleDependencySpec extends ZIOSpecDefault:
+  def spec = test("simple dependency") {
+    val aZIO: ZIO[Int, Nothing, Int] = ZIO.succeed(42)
+    assertZIO(aZIO)(equalTo(42))
+  }.provide(ZLayer.succeed(10))
+
+// example: a user survey application processing user data, fetching data from a database
+object BusinessLogicSpec extends ZIOSpecDefault:
+
+  // dependency
+  abstract class Database[K, V]:
+    def get(key: K): Task[V]
+    def put(key: K, value: V): Task[Unit]
+
+  object Database:
+    def create(url: String): UIO[Database[String, String]] = ??? // the REAL thing
+
+  // business logic under test
+  def normalizeUsername(name: String): UIO[String] = ZIO.succeed(name.toUpperCase())
+
+  val mockDatabase = ZIO.succeed(new Database[String, String] {
+    import scala.collection.mutable
+    val map = mutable.Map[String, String]()
+
+    def get(key: String): Task[String]              = ZIO.attempt(map(key))
+    def put(key: String, value: String): Task[Unit] = ZIO.attempt(map += key -> value)
+  })
+
+  def spec = suite("a user survey application should...")(
+    test("normalize user names") {
+      val surveyPreliminaryLogic =
+        for
+          db         <- ZIO.service[Database[String, String]]
+          _          <- db.put("123", "Daniel")
+          username   <- db.get("123")
+          normalized <- normalizeUsername(username)
+        yield normalized
+      assertZIO(surveyPreliminaryLogic)(equalTo("DANIEL"))
+    }
+  ).provide(ZLayer.fromZIO(mockDatabase))
+
+/*
+  built-in test services
+  - console
+  - random
+  - clock
+  - system
+ */
+object DummyConsoleApplication:
+  def welcomeUser(): Task[Unit] =
+    for
+      _    <- Console.printLine("Please enter your name...")
+      name <- Console.readLine
+      _    <- Console.printLine(s"Welcome, $name!")
+    yield ()
+
+object BuiltInTestServicesSpec extends ZIOSpecDefault:
+  def spec = suite("Checking built-in test services")(
+    test("ZIO console application") {
+      val logicUnderTest: Task[Vector[String]] =
+        for
+          _      <- TestConsole.feedLines("Daniel")
+          _      <- DummyConsoleApplication.welcomeUser()
+          output <- TestConsole.output
+        yield output.map(_.trim)
+
+      assertZIO(logicUnderTest)(
+        hasSameElements(List("Please enter your name...", "Welcome, Daniel!"))
+      )
+    },
+    test("ZIO clock") {
+      val parallelEffect =
+        for
+          fiber  <- ZIO.sleep(5.minutes).timeout(1.minute).fork
+          -      <- TestClock.adjust(1.minute)
+          result <- fiber.join
+        yield result
+
+      assertZIO(parallelEffect)(isNone)
+    },
+    test("ZIO random") {
+      val effect =
+        for
+          _     <- TestRandom.feedInts(3, 4, 1, 2)
+          value <- Random.nextInt
+        yield value
+
+      assertZIO(effect)(equalTo(3))
+    }
+  )
+
+/*
+  Test aspects
+ */
+object AspectsSpec extends ZIOSpecDefault:
+  def computeMeaningOfLife: UIO[Int] =
+    ZIO.sleep(2.seconds) *> ZIO.succeed(42)
+
+  def spec = suite("Testing Aspects")(
+    test("timeout aspect") {
+      val effect =
+        for
+          molFib <- computeMeaningOfLife.fork
+          _      <- TestClock.adjust(3.seconds)
+          value  <- molFib.join
+        yield value
+
+      assertZIO(effect)(equalTo(42))
+    } @@ timeout(10.millis)
+
+    /*
+      - timeout(duration) - REAL execution time
+      - eventually - retries until successful
+      - nonFlaky(n) - repeats n times, stops at first failure
+      - repeat(n) - the same
+      - retries(n) - retries n times, stop at first success
+      - debug - prints everything in the console
+      - silent - prints nothing
+      - diagnose(duration) - prints fiber dump if fails
+      - parallel/sequential (aspects of a SUITE)
+      - ignore
+      - success - fail all ignored tests
+      - timed - measure execution time
+      - before/beforeALl + after/afterALl
+     */
+  )
