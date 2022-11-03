f5a79de931954662b16da27ad0e119fe1e87f784
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-11-03 09:35:34 +0100

    ZIO Testing with JUnit

---
+++ src/main/scala/com/rockthejvm/part5testing/JUnitCompatibleSpec.scala
@@ -0,0 +1,30 @@
+package com.rockthejvm.part5testing
+
+import zio.*
+import zio.test.*
+
+// NOTE: I do not know how to make it work on vscode
+class JUnitCompatibleSpec extends junit.JUnitRunnableSpec:
+  def spec = suite("Full suite of tests")(
+    // pass multiple tests as arguments
+    test("simple test") {
+      assertTrue(1 + 3 == 4)
+    },
+    test("a second test") {
+      assertZIO(ZIO.succeed("Scala"))(
+        Assertion.hasSizeString(Assertion.equalTo(5)) && Assertion.startsWithString("S")
+      )
+    },
+    // sub-suites
+    suite("a nested suite")(
+      test("a nested test") {
+        assert(List(1, 2, 3))(Assertion.isNonEmpty && Assertion.hasSameElements(List(3, 1, 2)))
+      },
+      test("another nested test") {
+        assert(List(1, 2, 3).headOption)(Assertion.equalTo(Some(1)))
+      }
+      // test("a failed nested test") {
+      //   assertTrue(1 + 1 == 100)
+      // }
+    )
+  )
